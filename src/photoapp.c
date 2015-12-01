fecac12068f34cbdb9d1499f2213e85f1a1f52ff
Mukandila Mukandila <mukandila.m@gmail.com>
2015-11-30 22:58:42 -0500

    First commit

---
+++ src/photoapp.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : photoapp.c
+ Author      : Mukandila Mukandila
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gphoto2/gphoto2-camera.h>
+
+int main(void) {
+	printf("===Beginning camera detection===\n");
+	return EXIT_SUCCESS;
+}
