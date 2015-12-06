d8b7c6e05f1483ba0df7053fc83f7aa2adb5e23a
Mukandila Mukandila <mukandila.m@gmail.com>
2015-12-06 10:37:34 -0500

    A few modifications

--- src/photoapp.c
+++ src/photoapp.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : photoapp.c
- Author      : Mukandila Mukandila
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
