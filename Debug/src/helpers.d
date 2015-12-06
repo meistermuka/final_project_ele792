c7e83cf168d95a108bbbf07eab4f33649b42809e
Mukandila Mukandila <mukandila.m@gmail.com>
2015-12-06 10:42:16 -0500

    adding debug folder

---
+++ Debug/src/helpers.d
@@ -0,0 +1,43 @@
+src/helpers.d: ../src/helpers.c \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-camera.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-abilities-list.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-context.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-list.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-info-list.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-portability.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-log.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-widget.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-filesys.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-file.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-result.h \
+ /user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-result.h \
+ ../src/helpers.h
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-camera.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-abilities-list.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-context.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-list.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-info-list.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-portability.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-log.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-widget.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-filesys.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-file.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-result.h:
+
+/user/local/include/gphoto2/gphoto2/gphoto2/gphoto2-port-result.h:
+
+../src/helpers.h:
